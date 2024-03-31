94bbc04c2be28bfe3f45db208ee6cd35aeaa5e55
Sudhir Takale <sudhirtakale99@gmail.com>
2024-03-31 11:03:07 +0530

    added appoint

--- src/App.js
+++ src/App.js
@@ -5,30 +5,64 @@ import Dashboard from "./component/patient/Home/Dashboard";
 import ShowRecord from "./component/patient/HealthRecord/ShowRecord";
 
 import Appointment from "./component/patient/PatientAppointment/MainHome";
-import Confirmed from "./component/patient/PatientAppointment/Confirmed";
-
-import Pending from "./component/patient/PatientAppointment/Pending";
-
-import PatientRegister from "./component/patient/Forms/PatientRegistration";
+import Registration from "./component/patient/Forms/PatientRegistration";
 import AdminRoutes from "./component/Admin/AdminRoutes";
 import Login from "./component/auth/Login";
-import PatientRegistration from "./component/patient/Forms/PatientRegistration";
-import Doctor_Dashboard from "./component/doctor/dashboard/Doctor_Dashboard";
+// import Doctor_Dashboard from "./component/doctor/dashboard/Doctor_Dashboard";
 import AppointmentStats from "./component/doctor/AppointmentStats/AppointmentStats";
 import AppointmentMgmt from "./component/doctor/AppointmentManagement/AppointmentMgmt";
 import PatientHistory from "./component/doctor/PatientHistory/PatientHistory";
 import PatientList from "./component/doctor/PatientList/PatientList";
 import DoctorRegistration from "./component/doctor/Registration/DoctorRegistration";
 import UpdatePHR from "./component/doctor/UpdatePHR/UpdatePHR";
-import AccessPhr from "./component/patient/HealthRecord/AccessPhr";
+import AppointmentLayout from "./component/patient/PatientAppointment/AppointmentLayout";
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
+          <Route path="/admin" element={<AdminRoutes />} />
+
+          <Route path="/" element={<Home />} />
+          {/* Login and Registration */}
+
+          <Route path="/patient-registration" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+
+          {/* Patient Rotes */}
+
+          <Route path="/patient" element={<Dashboard />} />
+          <Route
+            path="/patient/manage-appointment"
+            element={<AppointmentLayout />}
+          >
+            <Route
+              path="/patient/manage-appointment/appointments"
+              element={<div>Hii iam all appointment</div>}
+            />
+            <Route
+              path="/patient/manage-appointment/confirmed"
+              element={<div>Hii iam confirm</div>}
+            />
+            <Route
+              path="/patient/manage-appointment/pending"
+              element={<div>Hii iam pending</div>}
+            />
+            <Route
+              path="/patient/manage-appointment/cancelled"
+              element={<div>Hii iam cancelled</div>}
+            />
+          </Route>
+          <Route path="/patient/telehealth" element={<Appointment />} />
+          <Route path="/patient/health-record" element={<ShowRecord />} />
+          <Route path="/patient/view-history" element={<Appointment />} />
+          <Route path="/patient/profile" element={<Appointment />} />
+          <Route path="/patient/setting" element={<Appointment />} />
+          <Route path="/patient/notification" element={<Appointment />} />
+
           {/* Doctor Routes  */}
-          <Route path="/doctor/dashboard" element={<Doctor_Dashboard />} />
+          {/* <Route path="/doctor/dashboard" element={<Doctor_Dashboard />} /> */}
           <Route path="/doctor-registration" element={<DoctorRegistration />} />
           <Route
             path="/doctor/appointment-stats"
